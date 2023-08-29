af588967f8c224eedb99150cb752f92c82c55541
blckmn <blackman@xtra.com.au>
2023-08-30 04:01:14 +1000

    Adding TBSAT32AIO (to be named)

---
+++ configs/TBSAT32AIO/config.h
@@ -0,0 +1,108 @@
+/*
+ * This file is part of Betaflight.
+ *
+ * Betaflight is free software. You can redistribute this software
+ * and/or modify this software under the terms of the GNU General
+ * Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Betaflight is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this software.
+ *
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#define FC_TARGET_MCU                       AT32F435M
+
+#define BOARD_NAME                          TBSAT32AIO
+#define MANUFACTURER_ID                     TEBS
+
+#define USE_ACC
+#define USE_ACC_SPI_ICM42688P
+
+#define USE_GYRO
+#define USE_GYRO_SPI_ICM42688P
+#define USE_SPI_GYRO
+
+#define GYRO_1_EXTI_PIN                     PB2
+#define GYRO_1_CS_PIN                       PA4
+#define GYRO_1_SPI_INSTANCE                 SPI1
+
+#define USE_BARO
+#define USE_BARO_BMP388
+
+#define USE_FLASH
+#define USE_FLASH_M25P16
+#define FLASH_CS_PIN                        PA15
+#define FLASH_SPI_INSTANCE                  SPI3
+
+#define USE_MAX7456
+#define MAX7456_SPI_CS_PIN                  PB12
+#define MAX7456_SPI_INSTANCE                SPI2
+
+#define BEEPER_PIN                          PC13
+#define MOTOR1_PIN                          PA8
+#define MOTOR2_PIN                          PA9
+#define MOTOR3_PIN                          PA10
+#define MOTOR4_PIN                          PA3
+
+#define LED_STRIP_PIN                       NONE
+
+#define UART1_TX_PIN                        PB6
+#define UART1_RX_PIN                        PB7
+
+#define UART2_TX_PIN                        PA2
+#define UART2_RX_PIN                        NONE
+
+#define UART3_TX_PIN                        PC10
+#define UART3_RX_PIN                        PC11
+
+#define UART4_TX_PIN                        NONE
+#define UART4_RX_PIN                        PH2
+
+#define UART5_TX_PIN                        PB9
+#define UART5_RX_PIN                        PB8
+
+#define I2C1_SCL_PIN                        PA0
+#define I2C1_SDA_PIN                        PA1
+
+#define LED0_PIN                            PH3
+
+#define SPI1_SCK_PIN                        PA5
+#define SPI1_SDI_PIN                        PA6
+#define SPI1_SDO_PIN                        PA7
+
+#define SPI2_SCK_PIN                        PB13
+#define SPI2_SDI_PIN                        PB14
+#define SPI2_SDO_PIN                        PB15
+
+#define SPI3_SCK_PIN                        PB3
+#define SPI3_SDI_PIN                        PB4
+#define SPI3_SDO_PIN                        PB5
+
+#define ADC_VBAT_PIN                        NONE
+#define ADC_CURR_PIN                        NONE
+
+#define PINIO1_PIN                          NONE
+
+//#define TIMER_PIN_MAPPING
+
+#define ADC1_DMA_OPT                        0
+#define SPI3_TX_DMA_OPT                     5
+
+#define ADC_INSTANCE                        ADC1
+#define BARO_I2C_INSTANCE                   (I2CDEV_1)
+
+#define DEFAULT_BLACKBOX_DEVICE             BLACKBOX_DEVICE_FLASH
+
+#define DEFAULT_CURRENT_METER_SOURCE        CURRENT_METER_ADC
+#define DEFAULT_VOLTAGE_METER_SOURCE        VOLTAGE_METER_ADC
